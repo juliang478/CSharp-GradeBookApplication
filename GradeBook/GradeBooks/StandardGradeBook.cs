94028cb95693eb3dd6bb23ef4cb70d0165f3385e
Julian Galvan <juliang478@email.phoenix.edu>
2019-11-03 10:03:01 -0800

    Completed module:

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+
+        }
+    }
+}
